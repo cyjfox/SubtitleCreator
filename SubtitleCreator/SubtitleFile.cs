ccedb67aff462651dd8b9c452c88695d502db494
cyjfox cyjfox <cyjfox cyjfox>
2017-09-05 18:22:14 +0800

    Signed-off-by: cyjfox cyjfox <cyjfox cyjfox>

---
+++ SubtitleCreator/SubtitleFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SubtitleCreator
+{
+    class SubtitleFile
+    {
+        //字幕行列表
+        List<SubtitleLine> subtileLines;
+        //字幕行总数
+        Int32 subtitleLineCount;
+        SubitleFileFormat subtitleFileFormat;
+        //字幕类型名称
+        string typeName;
+        //字幕文件全路径
+        string filePath;
+        //字幕文件名称
+        string fileName;
+
+    }
+}
---
+++ SubtitleCreator/SubtitleFileFormat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SubtitleCreator
+{
+
+    class SubtitleFileFormat
+    {
+        public enum SubtitleFileFormatCode
+        {
+            FORMATCODE_SUB
+        }
+
+        private SubtitleFileFormatCode subtitleFileFormatCode;
+
+        public SubtitleFileFormat()
+        {
+            Init(SubtitleFileFormatCode.FORMATCODE_SUB);
+        }
+
+        public SubtitleFileFormat(SubtitleFileFormatCode subtitleFileFormatCode)
+        {
+            Init(subtitleFileFormatCode);
+        }
+
+        private void Init(SubtitleFileFormatCode subtitleFileFormatCode)
+        {
+            this.subtitleFileFormatCode = subtitleFileFormatCode;
+        }
+
+        public static const SubtitleFileFormatCode FORMAT_SUB = SubtitleFileFormatCode.FORMATCODE_SUB;
+    }
+    
+    /*
+    public enum SubitleFileFormat
+    {
+        FORMAT_SUB
+    }
+    */
+}
